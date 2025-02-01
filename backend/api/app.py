e8796e4aa25bec1f68aee30a88a77ff685bb1cf5
anna1812 <adavies@connect.kellettschool.com>
2025-02-01 22:28:47 +0800

    Translation file structure layout.

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from neural_network.load_and_predict import load_and_predict
-from translation.translator import translate_sentence
+from translation.translator import Translator
 
 app = Flask(__name__)
 CORS(app)
@@ -25,7 +25,8 @@ def translate():
         data = request.get_json()
         text = data.get('text', '')
         
-        translation_sequence = translate_sentence(text) 
+        translator = Translator()
+        translation_sequence = translator.translate_sentence(text) 
         
         return jsonify({'signs': translation_sequence})
         
--- backend/translation/translator.py
+++ backend/translation/translator.py
@@ -1,3 +1,37 @@
-#This is currently connected to the app.py api. 
-def translate_sentence(): 
-    pass
+class Translator: 
+    
+    def __init__(self): 
+        self.word_mappings = self.load_mappings()
+        
+        def load_mappings(self): 
+            #Load from file. Use custom built dictionary class. 
+            pass
+
+        def translate_word(self, word): 
+            #get here is a dictionary built-in function, needs to be implemented with own dictionary class. 
+            return self.word_mappings.get(word.lower(), None)
+        
+        def translate_sentence(self, gloss_sentence): 
+            #gloss_sentence should already be a list of words in correct order. 
+            #NOTE: Should also add some sort of space sign in between words. 
+            #NOTE: All uses of dictionaries need to be replaced with dicionary built from scratch. 
+            #NOTE: Could replace for letter in word with some sort of queue process? 
+            translated_sequence = []
+            
+            for word in gloss_sentence: 
+                translation = self.translate_word(word)
+                if translation: 
+                    translated_sequence.append({
+                        'type': 'video' if translation.endswith('.mp4') else 'image', 
+                        'value': translation, 
+                        'label': word
+                    })
+                else: 
+                    for letter in word: 
+                        translated_sequence.append({
+                            'type': 'image',
+                            'value': f'{letter.lower()}.jpg',
+                            'label': letter
+                        })
+            
+            return translated_sequence
