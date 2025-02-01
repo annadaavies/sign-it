2c508cc5f853586794df0f68ed9e5b2a411b5f66
anna1812 <adavies@connect.kellettschool.com>
2025-02-01 22:34:05 +0800

    Correct logo file path

--- backend/neural_network/load_and_predict.py
+++
@@ -1,6 +0,0 @@
-from neural_network import Model 
-import numpy
-
-def load_and_predict(): 
-    #load the model and use the predict method to output prediction. Currently not training correctly so just a placeholder function at the minute.
-    return numpy.random.choice(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
